8c975b80c3930b5173d08c0be230a3e055495eab
Hamzah Shakeel <hamzah.shakeel.external@atos.net>
2025-10-07 11:44:11 +0100

    Reverted govuk-frontend 5.11.1 upgrade (#16)
    * Revert "Updated template and css to use latest govuk-frontend 5.11.1 styling (#15)"
    
    This reverts commit 88ba282d350cd8fe25b689bb4f669dfe121edb38.
    
    * Revert "updated to use latest govuk-frontend (#14)"
    
    This reverts commit 9a91eec46ed6e1a144fb8231fd8b1dc14d6ab231.

--- src/Project/gulpfile.js
+++ src/Project/gulpfile.js
@@ -1,35 +1,15 @@
 /// <binding BeforeBuild='build-frontend' />
 
-let enviroments = 'local,dev,tst,pre,prd';
+let enviroments = 'dev,tst,pre,prd';
 let enviromentList = enviroments.split(",");
 
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const replace = require('gulp-replace');
-const path = require('path');
-
-const storageContainerPath_local = './'
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const replace = require('gulp-replace')
 const storageContainerPath_dev = 'https://devrwddbssa1402.blob.core.windows.net/epr-registration-styling-container'
 const storageContainerPath_tst = 'https://tstrwddbssa1402.blob.core.windows.net/b2c-styling-files'
 const storageContainerPath_pre = 'https://prerwddbssa1402.blob.core.windows.net/epr-account-styling'
 const storageContainerPath_prd = 'https://prdrwddbssa1402.blob.core.windows.net/epr-account-styling'
-
-const deprecationSuppressions = ["import", "mixed-decls", "global-builtin"];
-const paths = {
-    "govuk": "node_modules/govuk-frontend/dist/govuk/"
-};
-
-let loadPaths = [
-    path.join(__dirname, "node_modules"),
-    path.join(__dirname, paths.govuk)
-];
-
-const sassOptions = {
-    loadPaths: loadPaths,
-    outputStyle: 'compressed',
-    quietDeps: true,
-    silenceDeprecations: deprecationSuppressions
-};
 
 enviromentList.forEach(CreateContent);
 
@@ -40,53 +20,37 @@ function CreateContent(environment) {
 
     gulp.task('compile-scss-' + environment, () => {
         return gulp.src('assets/scss/application.scss')
-            .pipe(sass(sassOptions, ''))
+            .pipe(sass({ outputStyle: 'compressed' }, ''))
             .pipe(gulp.dest(path, { overwrite: true }))
     })
 
     gulp.task('copy-fonts-' + environment, () => {
-        return gulp.src('node_modules/govuk-frontend/dist/govuk/assets/fonts/*')
+        return gulp.src('node_modules/govuk-frontend/govuk/assets/fonts/*')
             .pipe(gulp.dest(path, { overwrite: true }))
     })
 
     gulp.task('copy-images-' + environment, () => {
-        return gulp.src('node_modules/govuk-frontend/dist/govuk/assets/images/*')
+        return gulp.src('node_modules/govuk-frontend/govuk/assets/images/*')
             .pipe(gulp.dest(path, { overwrite: true }))
     })
 
     gulp.task('replace-css-paths-' + environment, () => {
+        return gulp.src([path + '/application.css'])
+            .pipe(replace('REPLACE-THIS/fonts', storageContainerPath))
+            .pipe(replace('REPLACE-THIS/images', storageContainerPath))
+            .pipe(gulp.dest(path, { overwrite: true }))
+    })
 
-        // Conditionally strip leading slash if environment is local
-        const adjustedPath = (environment === 'local' && storageContainerPath.startsWith('/'))
-            ? storageContainerPath.slice(1)
-            : storageContainerPath;
+    gulp.task('replace-template-paths-' + environment, () => {
+        return gulp.src(['assets/templates/SignUpSignIn.html'])
+            .pipe(replace('REPLACE-THIS-CSS-PATH', storageContainerPath))
+            .pipe(gulp.dest(path, { overwrite: true }))
+    })
 
-         return gulp.src([path + '/application.css'])
-             .pipe(replace('REPLACE-THIS/fonts', adjustedPath))
-             .pipe(replace('REPLACE-THIS/images', adjustedPath))
-             .pipe(gulp.dest(path, { overwrite: true }))
-     })
 
-     gulp.task('replace-template-paths-' + environment, () => {
-         return gulp.src(['assets/templates/SignUpSignIn.html'])
-             .pipe(replace('REPLACE-THIS-CSS-PATH', storageContainerPath))
-             .pipe(replace('REPLACE-THIS-REBRAND-PATH', storageContainerPath))
-             .pipe(gulp.dest(path, { overwrite: true }))
-     })
-
-    gulp.task('copy-rebrand-' + environment, () => {
-        return gulp.src('node_modules/govuk-frontend/dist/govuk/assets/rebrand/**/*', { base: 'node_modules/govuk-frontend/dist/govuk/assets/rebrand/' })
-            .pipe(gulp.dest(path + '/rebrand', { overwrite: true }));
-    });
-
-    gulp.task('build-frontend-' + environment,
-        gulp.series('compile-scss-' + environment,
-            'copy-fonts-' + environment,
-            'copy-images-' + environment,
-            'replace-css-paths-' + environment,
-            'replace-template-paths-' + environment,
-            'copy-rebrand-' + environment));
+    gulp.task('build-frontend-' + environment, gulp.series('compile-scss-' + environment, 'copy-fonts-' + environment, 'copy-images-' + environment, 'replace-css-paths-' + environment, 'replace-template-paths-' + environment))
 
 }
 
-gulp.task('build-frontend', gulp.series('build-frontend-local', 'build-frontend-dev', 'build-frontend-tst', 'build-frontend-pre', 'build-frontend-prd'))
+gulp.task('build-frontend', gulp.series('build-frontend-dev', 'build-frontend-tst', 'build-frontend-pre', 'build-frontend-prd'))
+
